09ac73feda2086afb979d18912726042c3fe0302
spezi77 <spezi77@gmx.com>
2016-11-19 13:13:26 +0100

    Fix FC in Navbar settings. * need to point to com.n4e.settings
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/systemui/navigation/BaseEditor.java
+++ src/com/android/systemui/navigation/BaseEditor.java
@@ -50,9 +50,9 @@ import android.widget.Toast;
 public abstract class BaseEditor implements Editor {
 
     public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
-    public static final String INTENT_ACTION_EDIT_COMPONENT = "com.nitrogen.settings.ActionPickerDialogActivity";
-    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.nitrogen.settings.IconPickerActivity";
-    public static final String INTENT_ACTION_GALLERY_PICKER_COMPONENT = "com.nitrogen.settings.IconPickerGallery";
+    public static final String INTENT_ACTION_EDIT_COMPONENT = "com.n4e.settings.ActionPickerDialogActivity";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.n4e.settings.IconPickerActivity";
+    public static final String INTENT_ACTION_GALLERY_PICKER_COMPONENT = "com.n4e.settings.IconPickerGallery";
     public static final String INTENT_ICON_PICKER = "intent_icon_picker";
     public static final String INTENT_GALLERY_PICKER = "intent_gallery_picker";
     public static final String INTENT_ACTION_PICKER = "intent_action_action_picker";
